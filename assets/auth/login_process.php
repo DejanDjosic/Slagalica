2c8be047f7bf3534799ba4beaa90a6308a3fbbb1
Dejan Đošić <105932677+DejanDjosic@users.noreply.github.com>
2023-12-23 20:33:19 +0100

    Added recognizing hashed password

--- assets/auth/login_process.php
+++ assets/auth/login_process.php
@@ -1,29 +1,40 @@
 <?php
 if (isset($_SESSION['session_id'])) {
     echo "<script>alert(\"Već ste ulogovani!\")</script>";
-} 
-else if (empty($_POST['sifra']) || empty($_POST['mejl'])) {
+} else if (empty($_POST['sifra']) || empty($_POST['mejl'])) {
     echo "<script>alert(\"Morate uneti oba polja!\")</script>";
 } else {
     $mejl = $_POST['mejl'];
-    $sifra = $_POST['sifra'];
+    $unetaLozinka = $_POST['sifra'];
 
-    $query = "SELECT * FROM korisnik WHERE mejl = ? AND sifra = ?";
-    $stmt = $connection->prepare($query);
-    $stmt->bind_param("ss", $mejl, $sifra);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    $enkriptovanaLozinka = mysqli_query($connection, "SELECT sifra FROM korisnik WHERE mejl='$mejl'");
+    if ($enkriptovanaLozinka) {
+        while ($row = mysqli_fetch_assoc($enkriptovanaLozinka)) {
+            $stringValue = (string)$row['sifra'];
+        }
+    }
 
-    if ($result->num_rows == 1) {
-        $red = $result->fetch_assoc();
-        $_SESSION['session_id'] = 1;
-        $_SESSION['korIme'] = $red['ime'];
-        $_SESSION['id_korisnika'] = $red['korisnikID'];
 
-        header('Location: /slagalica-master/index.php');
-        exit();
+    if ($stringValue != $unetaLozinka) {
+
+        $query = "SELECT * FROM korisnik WHERE mejl = ? AND sifra = ?";
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param("ss", $mejl, $stringValue);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 1) {
+            $red = $result->fetch_assoc();
+            $_SESSION['session_id'] = 1;
+            $_SESSION['korIme'] = $red['ime'];
+            $_SESSION['id_korisnika'] = $red['korisnikID'];
+
+            header('Location: /slagalica-master/index.php');
+            exit();
+        } else {
+            echo "<script>alert(\"Pogrešan mejl ili lozinka!\")</script>";
+        }
     } else {
-        echo "<script>alert(\"Pogrešan mejl ili lozinka!\")</script>";
+        die($connection->error);
     }
 }
-?>
