a7882fdce1b4c871d08f5e6b6d2228aba3431965
Dejan Đošić <105932677+DejanDjosic@users.noreply.github.com>
2023-12-23 19:06:23 +0100

    Added checking existing email

--- assets/auth/registration_process.php
+++ assets/auth/registration_process.php
@@ -1,11 +1,18 @@
 <?php
+include('../../includes/connection.php');
+
 if (isset($_POST['dodavanje'])) {
     $ime = $_POST["ime"];
     $mejl = $_POST["mejl"];
     $lozinka1 = $_POST["lozinka1"];
     $lozinka2 = $_POST["lozinka2"];
 
-    if (empty($lozinka1) || empty($lozinka2) || empty($ime) || empty($mejl)) {
+    $proveraMejla = mysqli_query($connection, "SELECT * FROM korisnik WHERE mejl='$mejl'");
+    $brojRedova = mysqli_num_rows($proveraMejla);
+    
+    if ($brojRedova > 0) {
+        echo "<script>alert('Već postoji korisnik sa tim mejlom')</script>";
+    } else    if (empty($lozinka1) || empty($lozinka2) || empty($ime) || empty($mejl)) {
         echo "<script>alert('Morate uneti sva polja')</script>";
     } else if ($lozinka1 !== $lozinka2) {
         echo "<script>alert('Polja sa šiframa moraju da se poklapaju')</script>";
@@ -16,7 +23,7 @@ if (isset($_POST['dodavanje'])) {
             $queryMaxID = "SELECT MAX(korisnikID) AS max_id FROM korisnik";
             $resultMaxID = $connection->query($queryMaxID);
             $maxID = $resultMaxID->fetch_assoc();
-            $id = ($maxID['max_id'] !== null) ? $maxID['max_id'] + 1 : 1; 
+            $id = ($maxID['max_id'] !== null) ? $maxID['max_id'] + 1 : 1;
 
             $query = $connection->prepare("INSERT INTO korisnik (korisnikID, mejl, ime, sifra) VALUES (?, ?, ?, ?)");
             $query->bind_param('isss', $id, $mejl, $ime, $lozinka1);
@@ -27,5 +34,3 @@ if (isset($_POST['dodavanje'])) {
         }
     }
 }
-
-?>
